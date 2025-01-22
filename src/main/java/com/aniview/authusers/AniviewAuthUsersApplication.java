5d9e5c6076e18d93f6e017eaa86847d576616189
Giovanni_TWD <giovannijimenezmurciano@gmail.com>
2025-01-22 17:39:24 +0000

    Initial commit

---
+++ src/main/java/com/aniview/authusers/AniviewAuthUsersApplication.java
@@ -0,0 +1,13 @@
+package com.aniview.authusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniviewAuthUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniviewAuthUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aniview/authusers/AniviewAuthUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniview.authusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniviewAuthUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
